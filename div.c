00530b08f743f8176886e1d72bf8e18e37c5b88e
jerichomata <74013719+jerichomata@users.noreply.github.com>
2020-11-08 03:23:55 -0800

    next step is to divide using bitwise operators

--- div.c
+++ div.c
@@ -16,34 +16,68 @@ int convBten(char *n)
     
 // }
 
-int convBtwo(int num_to_conv)
+void convBtwo(int num_to_conv, int * array)
 {
-    int arr[32] = {};
     for (int count = 31; count >= 0; count--) {
         int mask = num_to_conv >> count;
         if (mask & 1) {
-            arr[count] = 1;
+            array[count] = 1;
         }
         else {
-            arr[count] = 0;
+            array[count] = 0;
         }
-        // printf("%d", arr[count]);
-        int num = arr[count];
-        return num;
     }
+}
+
+void divideBtwo(int * dividend, int * divisor)
+{
+    printf("%d", dividend[1]);
+
 }
 
 int main( int argc, char *argv[])
 {
     // int rem = 0, quotient = 0;
+    int arr_arg1[32];
+    int arr_arg2[32];
     if ( argc > 3 || argc < 3) {
        printf("Input 2 numbers please!\n");
     }
     else {
         printf("arg1 = %s, arg2 = %s\n", argv[1], argv[2]);
         int arg1_Bten = convBten(argv[1]);
-        int arg1_Btwo = convBtwo(arg1_Bten);
-        printf("%d", arg1_Btwo);
+        // printf("%d", arg1_Bten);
+        convBtwo(arg1_Bten, arr_arg1);
+        /*printf("%d", arr_arg1[0]);
+        printf("%d", arr_arg1[1]);
+        printf("%d", arr_arg1[2]);
+        printf("%d", arr_arg1[3]);
+        printf("%d", arr_arg1[4]);
+        printf("%d", arr_arg1[5]);
+        printf("%d", arr_arg1[6]);
+        printf("%d", arr_arg1[7]);
+        printf("%d", arr_arg1[8]);
+        printf("%d", arr_arg1[9]);
+        printf("%d\n", arr_arg1[10]); */
+        int arg2_Bten = convBten(argv[2]);
+        convBtwo(arg2_Bten, arr_arg2);
+        /*printf("%d", arr_arg2[0]);
+        printf("%d", arr_arg2[1]);
+        printf("%d", arr_arg2[2]);
+        printf("%d", arr_arg2[3]);
+        printf("%d", arr_arg2[4]);
+        printf("%d", arr_arg2[5]);
+        printf("%d", arr_arg2[6]);
+        printf("%d", arr_arg2[7]);
+        printf("%d", arr_arg2[8]);
+        printf("%d", arr_arg2[9]);
+        printf("%d\n", arr_arg2[10]);
+        */ 
+
+        divideBtwo(*arr_arg1, *arr_arg2);
+
+        // int arg1_Btwo = convBtwo(arg1_Bten);
+        // printf("%d", arg1_Btwo);
         // int arg2_Bten = convBten(argv[2]);
         // printf("%d", arg1_Btwo);
         // int arg2_Btwo = convBtwo(arg2_Btwo);
